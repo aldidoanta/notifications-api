e1ad51aeaae108691b5160d8c8f23f33dfa9249b
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-11-19 13:19:44 +0000

    If the content-type is missing or not set to application/json, accept the request as long as the json is valid. If json is not valid throw BadRequestError with appropriate message. If the schema.validate method is called with an empty object then throw exception for that.

--- app/schema_validation/__init__.py
+++ app/schema_validation/__init__.py
@@ -58,7 +58,7 @@ def validate_schema_date_with_hour(instance):
 
 def validate(json_to_validate, schema):
     if json_to_validate is None:
-        raise BadRequestError(message="Request body is empty or Content-Type header is not set to application/json.",
+        raise BadRequestError(message="Request body is empty.",
                               status_code=400)
     validator = Draft7Validator(schema, format_checker=format_checker)
     errors = list(validator.iter_errors(json_to_validate))
